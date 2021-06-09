f41375a3505f0e82f23376ef496faadeba4fede4
Paul Adams <paul@baggerspion.net>
2021-06-09 09:27:54 +0200

    Moving all the data gathering/computation to the client

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
+import getLogs from '../lib/history'
 import GitViz from '../components/GitViz'
 import { useRouter } from 'next/router'
-import useSWR from 'swr'
 
 export default function Home() {
     const router = useRouter()
     const { owner, repo } = router.query
 
     if(owner && repo) {
-        const {data, error} = useSWR(`/api/history?owner=${owner}&repo=${repo}`)
+        const {data, error} = getLogs(owner, repo)
 
         if(error) return <h1>Problem: {error}</h1>
         if(!data) return <p>Loading... (this might take some time)</p>
@@ -17,9 +17,7 @@ export default function Home() {
         )
     } else {
         return (
-            <>
             <p>You must specify 'owner' and 'repo' parameters in the URL.</p>
-            </>
         )
     }
 }
