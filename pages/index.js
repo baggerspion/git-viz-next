2a8633e3d9fc6385d6d7de175b0ee747cc3acd65
Paul Adams <paul@baggerspion.net>
2021-06-09 15:58:40 +0200

    This should run on Vercel properly

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,34 @@
-import getLogs from '../lib/history'
 import GitViz from '../components/GitViz'
-import { useRouter } from 'next/router'
 
-export default function Home() {
-    const router = useRouter()
-    const { owner, repo } = router.query
+export default function Home({ commits }) {
+    return (
+        <GitViz logs={commits} />
+    )
+}
 
-    if(owner && repo) {
-        const {data, error} = getLogs(owner, repo)
+export async function getServerSideProps(context) {
+    // Get the URL params
+    const { owner, repo } = context.query
 
-        if(error) return <h1>Problem: {error}</h1>
-        if(!data) return <p>Loading... (this might take some time)</p>
+    // Connect to GH
+    const { Octokit } = require("@octokit/rest")
+    const { paginateRest, composePaginateRest } = require("@octokit/plugin-paginate-rest")
+    const PagingOctokit = Octokit.plugin(paginateRest)
+    const octokit = new PagingOctokit({
+        auth: process.env.GH_TOKEN 
+    })
 
-        if(data) return (
-            <GitViz logs={data['result']} />
-        )
-    } else {
-        return (
-            <p>You must specify 'owner' and 'repo' parameters in the URL.</p>
-        )
+    let commits = new Array()
+    for await (const response of octokit.paginate.iterator(
+        'GET /repos/{owner}/{repo}/commits', {
+            owner,
+            repo,
+            per_page: 100
+        })) {
+            commits = commits.concat(response.data)
+        }
+        
+    return {
+        props: { commits }
     }
 }
